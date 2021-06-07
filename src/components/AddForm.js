2d03977f1f9b8b4b96c4dea7b85a287e9fdad08e
Christopher Nugroho <Chrisnulis@gmail.com>
2021-06-07 17:04:47 -0400

    Front End Starting

---
+++ src/components/AddForm.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+
+
+const AddForm = (props) => {
+  return (
+    <div>
+      <h1>This is Add Form</h1>
+    </div>
+  )
+}
+
+export default AddForm
